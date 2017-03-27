53a11bb9995999e9c880dc92c9fd5091be971750
Arpit Mittal <arpit@roofpik.com>
2017-03-27 19:32:58 +0530

    reviews updated

--- js/controller.js
+++ js/controller.js
@@ -161,23 +161,39 @@ app.controller('writeReviewCtrl', function($scope, $timeout, $http, $state) {
     $scope.review = {
         ratings: {}
     }
-
+    ratingTxtVal = ['Click to rate', 'Terrible', 'Poor', 'Average', 'Good', 'Excellent']
     $scope.ratingParams = [{
         name: 'Security',
-        id: 2
+        id: 2,
+        rtxt: ratingTxtVal[0]
     }, {
         name: 'Amenities',
-        id: 3
+        id: 3,
+        rtxt: ratingTxtVal[0]
     }, {
         name: 'Open and green areas',
-        id: 4
+        id: 4,
+        rtxt: ratingTxtVal[0]
     }, {
         name: 'Convenience of parking',
-        id: 5
+        id: 5,
+        rtxt: ratingTxtVal[0]
     }, {
         name: 'Infrastructure',
-        id: 6
+        id: 6,
+        rtxt: ratingTxtVal[0]
     }];
+
+
+
+    $scope.ratingTxt = {
+        overall: ratingTxtVal[0],
+        security: ratingTxtVal[0],
+        amenities: ratingTxtVal[0],
+        green: ratingTxtVal[0],
+        parking: ratingTxtVal[0],
+        infrastructure: ratingTxtVal[0]
+    }
     $scope.ratingsObject = {
         iconOnColor: 'rgb(43, 187, 173)', //Optional
         iconOffColor: 'rgb(140, 140, 140)', //Optional
@@ -191,18 +207,27 @@ app.controller('writeReviewCtrl', function($scope, $timeout, $http, $state) {
     $scope.ratingsCallback = function(rating, index) {
         if (index == 1) {
             $scope.review.overallRating = rating;
+            $scope.ratingTxt.overall = ratingTxtVal[rating];
         } else if (index == 2) {
             $scope.review.ratings.security = rating;
+            $scope.ratingParams[index - 2].rtxt = ratingTxtVal[rating];
         } else if (index == 3) {
             $scope.review.ratings.amenities = rating;
+            $scope.ratingParams[index - 2].rtxt = ratingTxtVal[rating];
         } else if (index == 4) {
-            $scope.review.ratings.openAndGreenAreas = rating;
+            $scope.review.ratings.green = rating;
+            $scope.ratingParams[index - 2].rtxt = ratingTxtVal[rating];
         } else if (index == 5) {
-            $scope.review.ratings.convenienceOfParking = rating;
+            $scope.review.ratings.parking = rating;
+            $scope.ratingParams[index - 2].rtxt = ratingTxtVal[rating];
         } else if (index == 6) {
             $scope.review.ratings.infrastructure = rating;
+            $scope.ratingParams[index - 2].rtxt = ratingTxtVal[rating];
         }
     };
+
+
+
 
 
     $scope.submitReview = function() {
@@ -210,49 +235,21 @@ app.controller('writeReviewCtrl', function($scope, $timeout, $http, $state) {
         var userReviewPath = '';
         var newKey = '';
         $scope.review.userName = $scope.user.displayName;
-        // $scope.review.userName = 'Anu Porwal';
         $scope.review.userId = $scope.user.uid;
-        // $scope.review.userId = '2cQ2XQ7w7pdT9WGq2nyGJhrPSOo2';
         $scope.review.blocked = false;
-        $scope.review.createdDate = new Date().getTime();
-        $scope.review.dataFormat = 1;
+        $scope.review.created = new Date().getTime();
+        $scope.review.updated = $scope.review.created;
         $scope.review.wordCount = ($scope.review.reviewText).length;
         $scope.review.source = 'website';
         $scope.review.status = 'uploaded';
         var updates = {};
-        $scope.userReviewData = {
-            cityId: $scope.cityId,
-            cityName: 'Gurgaon',
-            countryId: $scope.countryId,
-            reviewTitle: $scope.review.reviewTitle,
-            status: $scope.review.status,
-            createdDate: $scope.review.createdDate
-        }
-        if ($scope.selectedProject.type == 'residential') {
-            newKey = db.ref('reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/residential/' + $scope.selectedProject.id).push().key;
-            $scope.userReviewData.id = $scope.selectedProject.id;
-            $scope.userReviewData.name = $scope.selectedProject.name;
-            reviewPath = 'reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/residential/' + $scope.selectedProject.id + '/' + newKey;
-            userReviewPath = 'userReviews/' + $scope.review.userId + '/residential/' + newKey;
-        } else if ($scope.selectedProject.type == 'location') {
-            newKey = db.ref('reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/location/' + $scope.selectedProject.id).push().key;
-            $scope.userReviewData.id = $scope.selectedProject.id;
-            $scope.userReviewData.name = $scope.selectedProject.name;
-            reviewPath = 'reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/location/' + $scope.selectedProject.id + '/' + newKey;
-            userReviewPath = 'userReviews/' + $scope.review.userId + '/location/' + newKey;
-        } else if ($scope.selectedProject.type == 'locality') {
-            newKey = db.ref('reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/locality/' + $scope.selectedProject.id).push().key;
-            $scope.userReviewData.id = $scope.selectedProject.id;
-            $scope.userReviewData.name = $scope.selectedProject.name;
-            reviewPath = 'reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/locality/' + $scope.selectedProject.id + '/' + newKey;
-            userReviewPath = 'userReviews/' + $scope.review.userId + '/locality/' + newKey;
-        } else if ($scope.selectedProject.type == 'cghs') {
-            newKey = db.ref('reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/cghs/' + $scope.selectedProject.id).push().key;
-            $scope.userReviewData.id = $scope.selectedProject.id;
-            $scope.userReviewData.name = $scope.selectedProject.name;
-            reviewPath = 'reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/cghs/' + $scope.selectedProject.id + '/' + newKey;
-            userReviewPath = 'userReviews/' + $scope.review.userId + '/cghs/' + newKey;
-        }
+
+        newKey = db.ref('reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/residential/' + $scope.selectedProject.id).push().key;
+        $scope.userReviewData.id = $scope.selectedProject.id;
+        $scope.userReviewData.name = $scope.selectedProject.name;
+        reviewPath = 'reviews/country/' + $scope.countryId + '/city/' + $scope.cityId + '/residential/' + $scope.selectedProject.id + '/' + newKey;
+        userReviewPath = 'userReviews/' + $scope.review.userId + '/residential/' + newKey;
+
 
         if (Object.keys($scope.review.ratings).length == 0) {
             delete $scope.review.ratings;
